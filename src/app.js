040c7f43b978910370a53cf93ca43c183283167f
John Sokolowski <johndsoko@gmail.com>
2020-02-26 18:12:16 -0600

    added sanitizing and further testing

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const ArticlesService = require('./articles-service')
-
+const articlesRouter = require('./articles/articles-router')
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -16,34 +15,12 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/articles', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getAllArticles(knexInstance)
-        .then(articles => {
-            res.json(articles.map(article => ({
-               id: article.id,
-               title: article.title,
-               style: article.style,
-               content: article.content,
-               date_published: new Date(article.date_published),
-             })))
-        })
-        .catch(next)
-})
+app.get('/xss', (req, res) => {
+    res.cookie('secretToken', '1234567890');
+    res.sendFile(__dirname + '/xss-example.html');
+  });
 
-app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-        if(!article) {
-            return res.status(404).json({
-                error: { message: `Article doesn't exist` }
-            })
-        }
-        res.json(article)
-    })
-    .catch(next)
-})
+app.use('/articles', articlesRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
